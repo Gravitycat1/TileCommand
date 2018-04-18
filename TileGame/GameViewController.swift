1d50d5027953de30e584cd3620cfb4495c63565e
Isaacs Joshua <>
2018-04-17 20:36:38 -0400

    I have added a menu

--- TileGame/GameViewController.swift
+++ TileGame/GameViewController.swift
@@ -13,24 +13,44 @@ import GameplayKit
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        
+         let view = UIView()
+        view.backgroundColor = .black
+        view.sizeToFit()
+        
+        ///title
+        let title = UILabel(frame: CGRect(x: 320, y: 200, width: 200, height: 100))
+        title.text = "TILE COMMAND"
+        title.textColor = .white
+        view.addSubview(title)
+        
+        
+            ///button
+        let button = UIButton(frame: CGRect(x: 330, y: 800, width: 100, height: 50))
+        button.backgroundColor = .white
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("PLAY", for: .normal)
+        button.addTarget(self, action: #selector(self.PlayGame), for: .touchUpInside)
+        view.addSubview(button)
+        
+        self.view = view
     }
+    
+    @IBAction func PlayGame(){
+        let scene = SKScene(fileNamed: "GameScene")
+            // Set the scale mode to scale to fit the window
+        let skView = SKView()
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene?.scaleMode = .aspectFill
+        skView.presentScene(scene)
+        self.view = skView
+        
+    }
+    
+    
 
     override var shouldAutorotate: Bool {
         return true
--- TileGame/Menu.swift
+++ TileGame/Menu.swift
@@ -12,4 +12,30 @@ import GameplayKit
 
 class Menu: SKScene{
     
+    var PlayButton:SKShapeNode = SKShapeNode()
+    
+    override func didMove(to view: SKView) {
+        
+        
+        
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        
+       
+        
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
 }
